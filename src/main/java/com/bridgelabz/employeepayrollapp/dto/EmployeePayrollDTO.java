9192bce1cac928bf696d25a26b64cfabb9c0c6cb
mihirpatil0 <mihirrpatil@gmail.com>
2021-09-02 20:02:26 +0530

    [mihir] Added: not null and regexp pattern validation to field.

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,7 +1,16 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+
+    @NotEmpty(message = "Employee name cannot be null.")
+    @Pattern(regexp = "^[A-Z][a-zA-z\\s]{2,}$",message = "Employee name is Invalid")
     public String name;
+
+    @Min(value = 500, message = "Minimum wage should be more than 500")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
