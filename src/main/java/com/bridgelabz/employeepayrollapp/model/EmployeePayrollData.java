128134653cb352d73e5fba80ba4fad64a44e1795
mihirpatil0 <mihirrpatil@gmail.com>
2021-09-04 01:48:26 +0530

    [mihir] Added: new anotation that auto creates table in mysql.

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -2,25 +2,40 @@ package com.bridgelabz.employeepayrollapp.model;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import lombok.Data;
-
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
+@Entity
+@Table(name = "employee_payroll")
 public class EmployeePayrollData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "employee_id")
     private int employeeId;
+
+    @Column(name = "name")
     private String name;
     private long salary;
     private String gender;
     private LocalDate startDate;
     private String note;
     private String profilePic;
+
+    @ElementCollection
+    @CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "department")
     private List<String> departments;
 
     public EmployeePayrollData() {}
 
-    public EmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO) {
-        this.employeeId = empId;
+    public EmployeePayrollData(EmployeePayrollDTO employeePayrollDTO){
+        this.updateEmployeePayrollData(employeePayrollDTO);
+    }
+
+    public void updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
         this.gender = employeePayrollDTO.gender;
